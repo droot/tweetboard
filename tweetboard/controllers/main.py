24fd2fa535ec68c1db7f030320ed46ac323868ce
Sunil Arora <arora.sunil@gmail.com>
2012-01-15 01:24:57 +0530

    added new controller

---
+++ tweetboard/controllers/main.py
@@ -0,0 +1,16 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from tweetboard.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class MainController(BaseController):
+
+    def index(self):
+        # Return a rendered template
+        #return render('/main.mako')
+        # or, return a response
+        return 'Hello World'
